46d0cae2a7a52e8c19c3acaa66f6fb7fdb30a2ed
shahid afridi <shahidafridi.baymax@gmail.com>
2023-06-08 10:53:14 +0600

    creating the add note page

--- lib/pages/addnotepage.dart
+++ lib/pages/addnotepage.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'package:todoapp/pages/homepage.dart';
 
 class AddNotePage extends StatefulWidget {
   const AddNotePage({super.key});
@@ -9,8 +14,130 @@ class AddNotePage extends StatefulWidget {
 }
 
 class _AddNotePageState extends State<AddNotePage> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _priority = 'Low';
+  DateTime _date = DateTime.now();
+  String btnText = 'Add Note';
+  String titleText = 'Add Note';
+
+  TextEditingController _dateController = TextEditingController();
+
+  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  final List<String> _priorities = ['Low', 'medium', 'High'];
+
+  _handleDatePicker() async {
+    final DateTime? date = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _dateFormatter.format(date);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: GestureDetector(
+        onTap: () {},
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (_) => HomePage())),
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Add Note',
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                              labelText: 'Title',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0))),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          controller: _dateController,
+                          style: TextStyle(fontSize: 18.0),
+                          onTap: _handleDatePicker,
+                          decoration: InputDecoration(
+                              labelText: 'Date',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0))),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                        child: DropdownButtonFormField(
+                          items: _priorities.map((String priotity) {
+                            return DropdownMenuItem(
+                                value: priotity,
+                                child: Text(
+                                  priotity,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18.0),
+                                ));
+                          }).toList(),
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                              labelText: 'Priority',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              )),
+                          onChanged: (value) {
+                            setState(() {
+                              _priority = value.toString();
+                            });
+                          },
+                          value: _priority,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
